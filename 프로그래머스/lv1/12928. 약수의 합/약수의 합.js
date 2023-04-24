7dab03b5f2f0611423927ea503a7798e6164f707
hayoenn2 <dwd1205@naver.com>
2023-04-25 00:31:39 +0900

    [Add] lv1 : 약수의 합 풀이 추가

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,10 +1,15 @@
 function solution(n) {
-    let result = 0;
-    
-    for(let i = 1; i <= n; i++){
-        if(n % i === 0) {
-            result += i;
-        }
+  let result = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      result += i;
     }
-    return result;
+  }
+  return result;
 }
+
+/*
+n을 약수로 나눴을 때 나머지가 0이 나오는 것들을 찾으면 된다.
+그리고 그 i들을 result에 합해준 후 결과를 반환한다.
+*/
