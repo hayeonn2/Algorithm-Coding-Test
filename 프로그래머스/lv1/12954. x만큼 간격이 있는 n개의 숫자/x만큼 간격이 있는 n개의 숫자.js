8f2167b3914391767a7a834bdf16455f4289ffea
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-04-26 10:38:35 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.20 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -0,0 +1,17 @@
+function solution(x, n) {
+    const result = [];
+    
+    for(let i = 1; i <= n; i++){
+        result.push(x * i);
+    }
+    
+    return result;
+}
+
+
+//let result = [];
+    
+//     for(let i = 1; i <= n; i++){
+//         result.push((x * i));
+//     }
+//     return result;
