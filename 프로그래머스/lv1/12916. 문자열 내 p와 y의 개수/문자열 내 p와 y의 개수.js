df6194d0f4fc670add061764aa3cdc95b97a3091
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-04-26 10:50:55 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -0,0 +1,5 @@
+function solution(s){
+    return s.toLowerCase().split("p").length === s.toLowerCase().split("y").length;
+}
+
+  //return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
