8207369b69d61d453ff5695520c3c6bba259958f
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-04-28 20:10:10 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,12 @@
+function solution(phone_number) {
+    const arr = phone_number.split("");
+    const backNumber = arr.splice(-4).join("");
+    
+    return arr.map((el)=> el.replace(/[0-9]/g, "*")).join("")+backNumber;
+}
+
+//     const numArr = phone_number.split("");
+//     const backNumber = numArr.splice(phone_number.length-4, 4).join("");
+//     const frontNumber = numArr.join("").replace(/[0-9]/g, "*");
+    
+//     return frontNumber + backNumber;
