c3b101cb9a45554bcaafec5c0fc3a0a027e30c8d
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-04-28 23:45:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,32 @@
+function solution(s) {
+    const midStrIdx = Math.ceil(s.length / 2);
+    
+    return s.length % 2 === 0 ? s[midStrIdx-1]+s[midStrIdx] : s[midStrIdx-1];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var answer = '';
+//     if(s.length % 2 === 0){
+//         answer = s[s.length / 2 - 1] + s[s.length / 2]
+//     } else {
+//         answer = s[Math.floor(s.length/2)]
+//     }
+//     return answer;
