678b3299f0f32fb3c3d030dff95e2399b7c97bf5
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-04-28 23:59:36 +0900

    [level 1] Title: 내적, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,12 +1,14 @@
 function solution(a, b) {
-    let result = 0;
-    
-    for(let i = 0; i < a.length; i++){
-        result += a[i] * b[i];
-    }
-    
-    return result;
+    return a.reduce((acc, _, i) => acc += a[i]*b[i], 0);
 }
+
+// let result = 0;
+    
+//     for(let i = 0; i < a.length; i++){
+//         result += a[i] * b[i];
+//     }
+    
+//     return result;
 
 // let result = 0;
     
