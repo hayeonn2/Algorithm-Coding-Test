fe9b5f8bac547703c7b7bf9438d9631a2f7fbc49
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-05-12 23:39:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.18 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,26 +1,42 @@
 function solution(maps) {
-    const queue = [];
-    const n = maps.length;
-    const m = maps[0].length;
-    const moveX = [-1, 1, 0, 0];
-    const moveY = [0, 0, -1, 1];
+    const HEIGHT = maps.length;
+    const WIDTH = maps[0].length;
+    const moves = [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1]
+    ];
+    let result = -1;
     
-    queue.push([0, 0, 1]);
+    const getIsVaild = (y, x) => {
+        if(x >= 0 && y >= 0 && x < WIDTH && y < HEIGHT && maps[y][x] === 1) return true;
+    }
     
-    while(queue.length){
-        const [curY, curX, move] = queue.shift();
-        if(curY === n - 1 && curX === m -1 ) return move;
+    const bfs = (start, end) => {
+        const queue = [[start, end, 1]];
         
-        for(let i = 0; i < 4; i++){
-            const movedX = curX + moveX[i];
-            const movedY = curY + moveY[i];
+        while(queue.length){
+            const [curY, curX, curCount] = queue.shift();
             
-            if(movedX >= 0 && movedX < m && movedY >= 0 && movedY < n && maps[movedY][movedX] === 1){
-                queue.push([movedY, movedX, move + 1]);
-                maps[movedY][movedX] = 0;
+            if(curY === HEIGHT - 1 && curX === WIDTH - 1){
+                result = curCount;
+                return;
             }
+            
+            moves.forEach((move)=>{
+                const [dy, dx] = move;
+                const [ny, nx] = [curY + dy, curX + dx];
+                
+                if(getIsVaild(ny, nx)){
+                    queue.push([ny, nx, curCount+1]);
+                    maps[ny][nx] = 0;
+                }
+            })
         }
     }
     
-    return -1;
+    bfs(0, 0);
+    
+    return result;
 }
