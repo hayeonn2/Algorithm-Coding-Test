d898d1d989b56b904127063ba0c7e093a82b3405
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-06-02 12:56:32 +0900

    [level 2] Title: 모음 사전, Time: 1.06 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,26 +1,21 @@
 function solution(word) {
+    const vowels = ["A", "E", "I", "O", "U"]
     const result = [];
-    const str = "";
     
-    const dfs = (word, length, result) => {
-        const vowels = ["A", "E", "I", "O", "U"]
-        
-        if(length === word.length){
-            result.push(word);
-            
+    const dfs = (str, length) => {
+        if(str.length === length){
+            result.push(str);
             return;
         }
         
-        vowels.forEach((vowel)=>{
-            dfs(word + vowel, length, result);
+        vowels.forEach((vowel) => {
+            dfs(str+vowel, length);
         })
     }
     
     for(let i = 1; i <= 5; i++){
-        dfs(str, i, result);
+        dfs("", i)
     }
     
-    return result.sort().indexOf(word) + 1;
-    
-    
+    return result.sort().indexOf(word) + 1
 }
