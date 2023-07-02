156c02ea99728aabca74707834345e1c71d75db2
HaYeonKim (김하연) <111109573+hayeonn2@users.noreply.github.com>
2023-07-02 14:24:27 +0900

    [level 2] Title: 택배상자, Time: 82.47 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.js
@@ -0,0 +1,23 @@
+function solution(order) {
+    const stack = [];
+  let answer = 0;
+  
+  let idx = 0;
+
+  for (let i = 1; i <= order.length; i++) {
+    if (order[idx] !== i) {
+      stack.push(i);
+    } else {
+      idx++;
+      answer++;
+    }
+
+    while (stack.length !== 0 && stack.at(-1) === order[idx]) {
+      stack.pop();
+      idx++;
+      answer++;
+    }
+  }
+
+  return answer;
+}
