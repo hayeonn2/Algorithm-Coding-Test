660a25b9a63a88a013a3f60c12a09a4d45ee50e2
Hayeon Kim <111109573+hayeonn2@users.noreply.github.com>
2023-11-26 20:37:36 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    
+    return parseInt(n.toString(3).split("").reverse().join(""), 3)
+
+}
+
+    
+//     const trit = n.toString(3);
+//     const reversed = trit.split('').reverse().join('');
+//     const answer = parseInt(reversed, 3);
+    
+//     return answer;
