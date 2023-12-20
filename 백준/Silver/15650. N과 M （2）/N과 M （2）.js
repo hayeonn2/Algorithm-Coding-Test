bff20b7a1b774ab9354f09d263b89e8aef2958e3
Hayeon Kim <111109573+hayeonn2@users.noreply.github.com>
2023-12-20 18:44:37 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,30 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const seq = [...Array(m).fill(0)];
+const visited = [...Array(n).fill(false)];
+let result = "";
+
+function dfs(k, idx) {
+  const arr = [];
+
+  if (k === m) {
+    for (let i = 0; i < m; i++) {
+      arr.push(seq[i]);
+    }
+    return (result += `${arr.join(" ")}\n`);
+  }
+
+  for (let i = idx; i <= n; i++) {
+    if (!visited[i]) {
+      seq[k] = i;
+      visited[i] = true;
+      dfs(k + 1, i);
+      visited[i] = false;
+    }
+  }
+}
+
+dfs(0, 1);
+
+console.log(result);
