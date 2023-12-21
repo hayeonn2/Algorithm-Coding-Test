a7ca6af85732ed80f93e4c6f3719df8b12013a0d
Hayeon Kim <111109573+hayeonn2@users.noreply.github.com>
2023-12-21 23:50:58 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -4,7 +4,7 @@ function solution(citations) {
     citations.sort((a,b)=> b-a);
     
     for(let i = 0; i < citations.length; i++){
-        if( i< citations[i]){
+        if( i+1<=citations[i]){
             result++;
         }
     }
